6066ec64cb45ef4ea07adafcfaeb1d961089a160
foxsir <838394225@qq.com>
2025-08-12 21:42:26 +0800

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import {Button} from "@heroui/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div
       className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Button>Button</Button>
+      <Link href={'/hooks'} className={'!block !w-[100%]'}>
+        <Button className={'w-full'}>useHooks</Button>
+      </Link>
     </div>
   );
 }
